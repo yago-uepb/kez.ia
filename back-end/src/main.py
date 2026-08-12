3341a58ce19807a615a8edc196fa0274618ac62b
yago-uepb <yago.jordas@aluno.uepb.edu.br>
2026-08-12 19:37:57 -0300

    feat: permite a conexão com o Front-end

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,13 +1,15 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .core.database import close_db_connection, connect_db
+from .core.settings import settings
 from .features.ai.router import router as ai_routes
+from .features.list_problems.router import router as list_problems_routes
 from .features.lists.router import router as list_routes
 from .features.problems.router import router as problem_routes
 from .features.submits.router import router as submit_routes
-from .features.list_problems.router import router as list_problems_routes
 from .features.test_cases.router import router as test_cases_routes
 
 
@@ -21,6 +23,14 @@ app = FastAPI(
     title="kez.ia — API Documentation", 
     version="0.1.0", 
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(ai_routes)
